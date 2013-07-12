f639e7e5f6e6ce5812bbab405e9dd309a0994c4e
user <user@user-VirtualBox.(none)>
2013-07-12 16:57:14 +0200

    mensaje que responda a la llamada

--- web.js
+++ web.js
@@ -1,15 +1,18 @@
-#!/usr/bin/env node
+//#!/usr/bin/env node
 
 var express = require('express');
 
 var app = express(); //.createServer(express.logger());
 
+resonse.log("iniciando la aplicacion");
+
 app.get('/', function(request, response) {
   var fs = require('fs');
+
+  console.log("hemos recibido algo");
 //  var cad = fs.readFileSync('index.html','utf8');
   response.send("hola mundo");
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
