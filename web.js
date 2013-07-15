765f483e3bd4cff752fc490db2953b6b6278a1e5
user <user@user-VirtualBox.(none)>
2013-07-15 13:02:58 +0200

    os

--- web.js
+++ web.js
@@ -8,17 +8,17 @@ var my_function = function (request, response){
 	var cad;
 	var os = require ('os');
 	cad = "tmpdir:" + os.tmpdir() +"<br>";
-	cad = "endianness:" + os.endianness() +"<br>";
-	cad = "hostname:" + os.hostname() +"<br>";
-	cad = "type:" + os.type() +"<br>";
-	cad = "platform:" + os.platform() +"<br>";
-	cad = "arch:" + os.arch() +"<br>";
-	cad = "release:" + os.release() +"<br>";
-	cad = "uptime:" + os.uptime() +"<br>";
-	cad = "loadavg:" + os.loadavg() +"<br>";
-	cad = "totalmem:" + os.totalmem() +"<br>";
-	cad = "freemem:" + os.freemem() +"<br>";
-	cad = "cpus:" + os.cpus() +"<br>";
+	cad += "endianness:" + os.endianness() +"<br>";
+	cad += "hostname:" + os.hostname() +"<br>";
+	cad += "type:" + os.type() +"<br>";
+	cad += "platform:" + os.platform() +"<br>";
+	cad += "arch:" + os.arch() +"<br>";
+	cad += "release:" + os.release() +"<br>";
+	cad += "uptime:" + os.uptime() +"<br>";
+	cad += "loadavg:" + os.loadavg() +"<br>";
+	cad += "totalmem:" + os.totalmem() +"<br>";
+	cad += "freemem:" + os.freemem() +"<br>";
+	cad += "cpus:" + os.cpus() +"<br>";
 	response.send(cad);
 
 }
